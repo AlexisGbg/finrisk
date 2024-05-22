cb2fdece517d14045f4a31de00d9333d3b6c7c0a
deeps-ac2020 <62571202+deeps-ac2020@users.noreply.github.com>
2024-05-22 12:51:34 -0400

    Update side_bar.py

--- side_bar.py
+++ side_bar.py
@@ -43,6 +43,7 @@ def load_sidebar() -> None:
 
     model_tab.markdown("""
         Monte Carlo simulation employs random sampling to model a range of possible outcomes. By simulating numerous scenarios, it provides a comprehensive understanding of potential risks and rewards.
+        
         :green[VaR (Value at Risk)]: VaR functions as a safety net, indicating the maximum potential loss within a confidence level, such as a 95% chance of not losing more than $X. It equips you for worst-case scenarios, with alpha representing the confidence level (e.g., 5% implies 95% confidence).
 
  :green[Conditional Value at Risk (CVaR)]: CVaR extends beyond VaR, disclosing expected losses beyond the worst-case scenario. It serves as a contingency plan for extreme situations, with alpha denoting the confidence level (e.g., 5% implies 95% confidence).
