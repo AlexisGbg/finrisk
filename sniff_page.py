8f81ab2b492603802c99b36afa4180e7406de3c7
Alexis Guibourge <alexis.guibourge@siemens.com>
2024-05-22 09:03:32 -0400

    order the KPI list

--- sniff_page.py
+++ sniff_page.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.vars import INCOME_STMT_KPI, BALANCE_SHEET_KPI
+from src.vars import INCOME_STMT_KPI, BALANCE_SHEET_KPI, COMPUTED_KPI, ORDERED_KPIS
 import pandas as pd
 import plotly.graph_objects as go
 import random
@@ -10,6 +10,7 @@ GROWTH_RATE_MEAN_DEFAULT = 0.02
 GROWTH_RATE_STD_DEFAULT = 0.05
 GROWTH_RATE_LOWER_DEFAULT = -0.07
 GROWTH_RATE_UPPER_DEFAULT = 0.1
+
 
 def load_page():
     # KPIs
@@ -28,55 +29,54 @@ def load_page():
         columns = sorted(list(set(incomestmt.columns) & set(balance_sheet.columns)))
         data = pd.concat([incomestmt[columns], balance_sheet[columns]])
         # Adding ratios
-        data.loc["Leverage"] = (
+        data.loc[COMPUTED_KPI.LEVERAGE.value] = (
             data.loc[INCOME_STMT_KPI.EBITDA.value]
             / data.loc[BALANCE_SHEET_KPI.TOTAL_DEPT.value]
         )
-        data.loc["Enterprise Multiple"] = (
+        data.loc[COMPUTED_KPI.ENTERPRISE_MULTIPLE.value] = (
             data.loc[BALANCE_SHEET_KPI.TOTAL_CAPITALIZATION.value]
             / data.loc[INCOME_STMT_KPI.EBITDA.value]
         )
-        data.loc["EBITDA Margin"] = (
+        data.loc[COMPUTED_KPI.EBITDA_MARGIN.value] = (
             data.loc[INCOME_STMT_KPI.EBITDA.value]
             / data.loc[INCOME_STMT_KPI.REVENUE.value]
         ) * 100
-        data.loc["Revenue Growth"] = (
+        data.loc[COMPUTED_KPI.REVENUE_GROWTH.value] = (
             data.loc[INCOME_STMT_KPI.REVENUE.value].pct_change() * 100
         )
-        data.loc["EBITDA Growth"] = (
+        data.loc[COMPUTED_KPI.EBITDA_GROWTH.value] = (
             data.loc[INCOME_STMT_KPI.EBITDA.value].pct_change() * 100
         )
-        st.dataframe(data)
-    
+        st.dataframe(data.loc[[x.value for x in ORDERED_KPIS]])
 
-    st.subheader('Revenue growth')
-    radio_button_revenue_growth = st.radio('Choose growth rate distribution', ('Normal', 'Uniform'))
+    st.subheader("Revenue growth")
+    radio_button_revenue_growth = st.radio(
+        "Choose growth rate distribution", ("Normal", "Uniform")
+    )
 
     gr_mean = GROWTH_RATE_MEAN_DEFAULT
     gr_std = GROWTH_RATE_STD_DEFAULT
     gr_lower = GROWTH_RATE_LOWER_DEFAULT
     gr_upper = GROWTH_RATE_UPPER_DEFAULT
-    if radio_button_revenue_growth == 'Normal':
-        gr_mean = st.number_input('Mean revenue growth rate (in %)', value=gr_mean)
-        gr_std = st.number_input('Revenue growth rate std. dev. (in %)', value=gr_std)
-    elif radio_button_revenue_growth == 'Uniform':
-        gr_lower = st.number_input('Lower end growth rate (in %)', value=gr_lower)
-        gr_upper = st.number_input('Upper end growth rate (in %)', value=gr_upper)
-        
-
+    if radio_button_revenue_growth == "Normal":
+        gr_mean = st.number_input("Mean revenue growth rate (in %)", value=gr_mean)
+        gr_std = st.number_input("Revenue growth rate std. dev. (in %)", value=gr_std)
+    elif radio_button_revenue_growth == "Uniform":
+        gr_lower = st.number_input("Lower end growth rate (in %)", value=gr_lower)
+        gr_upper = st.number_input("Upper end growth rate (in %)", value=gr_upper)
 
     # Add Sniff test tab
     if st.button("Run Simulation"):
         fig = go.Figure()
         for _ in range(MONTE_CARLO_ITERATIONS):
-            if radio_button_revenue_growth == 'Normal':
+            if radio_button_revenue_growth == "Normal":
                 grs = [random.gauss(gr_mean, gr_std) for x in range(100)]
             else:
                 grs = [random.uniform(gr_lower, gr_upper) for x in range(100)]
             rev = data.loc[INCOME_STMT_KPI.REVENUE.value][-1]
             revs = []
             for gr in grs:
-                rev *= (1+gr)
+                rev *= 1 + gr
                 revs.append(rev)
             fig.add_trace(
                 go.Scatter(
